b45077285ac6e03abbad6363dc514ae645b1768a
Anas Naeem <huzammughal@gmail.com>
2022-11-04 21:06:16 +0500

    Invoces list added

--- src/screens/dashBoard/home/index.js
+++ src/screens/dashBoard/home/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
+import colors from '~/constants/colors';
+import {hp} from '~/constants/dimensions';
+import {InvoiceService} from '~/services';
 import styles from './styles';
 
 export const Home = props => {
+  const [invoices, setInvoices] = useState([]);
+
+  useEffect(() => {
+    InvoiceService.find().then(res => {
+      setInvoices(res.data.data);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <TextInput placeholder="Search invoice" style={styles.search} />
+      <FlatList
+        fla
+        style={{flex: 1}}
+        data={invoices}
+        keyExtractor={item => item._id}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item, index}) => {
+          console.log(item);
+          return (
+            <View>
+              <Text>{item?.type}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
--- src/screens/dashBoard/home/styles.js
+++ src/screens/dashBoard/home/styles.js
@@ -5,8 +5,15 @@ import colors from '../../../constants/colors';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: `${colors.background}`,
     paddingTop: hp(7),
+  },
+  search: {
+    height: hp(7),
+    borderRadius: 100,
+    marginHorizontal: 10,
+    fontSize: 14,
+    padding: 20,
+    backgroundColor: colors.background,
   },
 });
 
